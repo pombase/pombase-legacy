e8e5d8f11c1c1b413a31c6bcf5fdceacf88267fb
Kim Rutherford <kmr44@cam.ac.uk>
2013-10-09 17:12:06 +1300

    Revert "Remove unnecessary loop"
    This reverts commit d81ee36e5fe968ab3960b84be5202a9101f5328d.
    
    The loop wasn't actually unnecessary.

--- lib/PomBase/Chado/QualifierLoad.pm
+++ lib/PomBase/Chado/QualifierLoad.pm
@@ -826,11 +826,14 @@ method process_one_cc($chado_object, $bioperl_feature, $qualifier,
   }
 
   if (!defined $cv_name) {
-    if ($term =~ /^(.*), /) {
-      my $maybe_cv_name = $1;
-      $cv_name = $self->objs()->{cv_long_names}->{$maybe_cv_name};
-      $term =~ s/^$maybe_cv_name, //;
-    }
+    map {
+      my $long_name = $_;
+
+      if ($term =~ s/^$long_name, *//) {
+        my $short_cv_name = $self->objs()->{cv_long_names}->{$long_name};
+        $cv_name = $short_cv_name;
+      }
+    } keys %{$self->objs()->{cv_long_names}};
   }
 
   my $chado_object_type = $chado_object->type()->name();
