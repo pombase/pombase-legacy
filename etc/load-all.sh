76051ea6a4dfbf1998058591a13b6cc32d30c6cc
Kim Rutherford <kmr44@cam.ac.uk>
2014-06-02 01:06:08 +1200

    Add missing --gene-ex-qualifiers option

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -134,7 +134,7 @@ echo load bulk qualitative gene expression files
 for file in $SOURCES/pombe-embl/external_data/qualitative_gene_expression_data/*
 do
   echo loading: $file
-  $POMBASE_CHADO/script/pombase-import.pl load-pombase-chado.yaml qualitative $HOST $DB $USER $PASSWORD < $file 2>&1
+  $POMBASE_CHADO/script/pombase-import.pl load-pombase-chado.yaml qualitative --gene-ex-qualifiers $SOURCES/pombe-embl/supporting_files/gene_ex_qualifiers $HOST $DB $USER $PASSWORD < $file 2>&1
 done | tee $LOG_DIR/$log_file.qualitative
 
 
