cb9072476f54c689d82899d958f51147613e6431
Kim Rutherford <kmr44@cam.ac.uk>
2014-07-26 16:34:55 +1200

    Add new ext. rel. to test data and check FC count

--- lib/PomBase/Chado/CheckLoad.pm
+++ lib/PomBase/Chado/CheckLoad.pm
@@ -145,6 +145,11 @@ method check
   my @all_feature_cvterm = $chado->resultset('Sequence::FeatureCvterm')->all();
   should(scalar(@all_feature_cvterm), 103);
 
+  my $ext_feature_cvterm_rs =
+    $chado->resultset('Sequence::FeatureCvterm')->search({ 'cv.name' => 'PomBase annotation extension terms' },
+                                                         { join => { cvterm => 'cv' } });
+  should($ext_feature_cvterm_rs->count(), 14);
+
   my $cvterm_property_type_cv =
     $chado->resultset('Cv::Cv')->find({ name => 'cvterm_property_type' });
   my $cvtermprop_types_rs = $chado->resultset('Cv::Cvterm')->search({ cv_id => $cvterm_property_type_cv->cv_id(),
@@ -154,7 +159,7 @@ method check
 
   my $an_ex_rel_props_rs = $chado->resultset('Cv::Cvtermprop')->search({
     type_id => { -in => $cvtermprop_types_rs->get_column('cvterm_id')->as_query() } });
-  should($an_ex_rel_props_rs->count(), 4);
+  should($an_ex_rel_props_rs->count(), 5);
 
   my ($localizes_term) = grep { $_->cvterm()->name() =~ /cellular protein localization \[localizes\] SPAC167.03c/ } @all_feature_cvterm;
   assert(defined $localizes_term);
