64cff3b4ac14ac15ed2adf97d94275524c1821d7
Kim Rutherford <kmr44@cam.ac.uk>
2015-01-11 11:26:40 +1300

    Add --no-verbose flag for BioGRID fetching

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -46,7 +46,7 @@ echo starting import of biogrid data | tee $log_file.biogrid-load-output
 
 (cd $SOURCES/biogrid
 mv BIOGRID-* old/
-wget http://thebiogrid.org/downloads/archives/Latest%20Release/BIOGRID-ORGANISM-LATEST.tab2.zip
+wget --no-verbose http://thebiogrid.org/downloads/archives/Latest%20Release/BIOGRID-ORGANISM-LATEST.tab2.zip
 unzip -q BIOGRID-ORGANISM-LATEST.tab2.zip
 if [ ! -e BIOGRID-ORGANISM-Schizosaccharomyces_pombe-*.tab2.txt ]
 then
