3b09f7719ac81da8bee12ea45666b18dea5767bb
Kim Rutherford <kmr44@cam.ac.uk>
2017-09-03 18:37:03 +1200

    Update pombase-dev Docker service automatically

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -483,6 +483,8 @@ fi
 
 (cd ~/git/pombase-chado && nice -10 ./etc/build_container.sh ~/git/pombase-chado/etc/docker-conf $DB_DATE_VERSION $DUMPS_DIR/latest_build preview)
 nice -19 docker save pombase/web:$DB_DATE_VERSION-preview | ssh pombase-admin@149.155.131.177 sudo docker load
+
 (cd ~/git/pombase-chado && nice -10 ./etc/build_container.sh ~/git/pombase-chado/etc/docker-conf $DB_DATE_VERSION $DUMPS_DIR/latest_build dev)
+docker service update --image=pombase/web:$DB_DATE_VERSION-dev pombase-web
 
 date
