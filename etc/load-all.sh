c042ffdc8a30100986d442b3944b0ce66de854fc
Kim Rutherford <kmr44@cam.ac.uk>
2017-11-28 21:27:49 +1300

    Don't ignore the first line of the SGD gene file

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -64,13 +64,13 @@ $POMBASE_CHADO/script/pombase-import.pl $HOME/git/pombase-legacy/load-pombase-ch
 
 $POMBASE_CHADO/script/pombase-import.pl $HOME/git/pombase-legacy/load-pombase-chado.yaml features \
     --organism-taxonid=4896 --uniquename-column=1 --name-column=2 --feature-type=gene \
-    --ignore-first_line --ignore-short-lines \
+    --ignore-first-line --ignore-short-lines \
     "$HOST" $DB $USER $PASSWORD < $SOURCES/hgnc_complete_set.txt)
 
 $POMBASE_CHADO/script/pombase-import.pl $HOME/git/pombase-legacy/load-pombase-chado.yaml features \
     --organism-taxonid=4896 --uniquename-column=4 --name-column=5 \
     --column-filter="2=ORF" --feature-type=gene \
-    --ignore-first_line --ignore-short-lines \
+    --ignore-short-lines \
     "$HOST" $DB $USER $PASSWORD < $SOURCES/SGD_features.tab)
 
 cd $LOG_DIR
