e3cad9cce15cad378ce1a000d2d4cac4fcbcb866
Kim Rutherford <kmr44@cam.ac.uk>
2017-12-02 12:35:06 +1300

    Store all GGD features as genes with no transcript

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -79,7 +79,7 @@ do
   echo loading $so_type genes from SGD data file
   $POMBASE_CHADO/script/pombase-import.pl $HOME/git/pombase-legacy/load-pombase-chado.yaml features \
       --organism-taxonid=4932 --uniquename-column=4 --name-column=5 \
-      --column-filter="2=${so_type}_gene" --feature-type=$so_type \
+      --column-filter="2=${so_type}_gene" --feature-type=gene \
       --ignore-short-lines \
       "$HOST" $DB $USER $PASSWORD < $SOURCES/SGD_features.tab
 done
