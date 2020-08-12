5ddc7fce34690721b619fc6d1d81aa71dae2bdc4
Kim Rutherford <kmr@aska.gen.nz>
2020-08-12 21:56:18 +1200

    Add GO to ECO mapping flag
    Refs pombase/pombase-chado#744

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -613,7 +613,7 @@ psql $DB -c "select count(distinct fc_id) as total from $sub_query;"
 
 refresh_views
 
-$POMCUR/bin/pombase-chado-json -c ~/git/pombase-config/website/pombase_v2_config.json  --doc-config-file ~/git/pombase-website/src/app/config/doc-config.json -p "postgres://kmr44:kmr44@localhost/$DB" -d $CURRENT_BUILD_DIR/  -i /var/pomcur/sources/interpro/pombe_domain_results.json 2>&1 | tee $LOG_DIR/$log_file.web-json-write
+$POMCUR/bin/pombase-chado-json -c ~/git/pombase-config/website/pombase_v2_config.json  --doc-config-file ~/git/pombase-website/src/app/config/doc-config.json -p "postgres://kmr44:kmr44@localhost/$DB" -d $CURRENT_BUILD_DIR/ --go-evidence-mapping=$CURRENT_BUILD_DIR/ontologies/gaf-eco-mapping.txt  -i /var/pomcur/sources/interpro/pombe_domain_results.json 2>&1 | tee $LOG_DIR/$log_file.web-json-write
 
 gzip -r9 $CURRENT_BUILD_DIR/fasta
 
