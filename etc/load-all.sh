3c762c51420101ee01521e37750172a553a719ce
Kim Rutherford <kmr@aska.gen.nz>
2023-10-22 13:20:38 +1300

    Add message before POSTing to APICURON
    Refs pombase/pombase-chado#1013

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -1049,6 +1049,8 @@ echo build and deploy allele_qc container
  docker service update --image pombase/allele_qc:$DATE_VERSION allele_qc
  docker service update --replicas 1 allele_qc)
 
+echo
+echo sending data to APICURON:
 curl -X 'POST' \
   'https://dev.apicuron.org/api/reports/bulk' \
   -H 'accept: */*' \
