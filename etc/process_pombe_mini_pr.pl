98c8ff884db20bbed935659e7599606470bfbc9b
Kim Rutherford <kmr@aska.gen.nz>
2024-05-10 17:40:33 +1200

    Include original PRO term name as EXACT synonym
    We replace the PRO term name in the mini PRO OBO file with
    the EXACT PomBase_display_name synonym.  Instead of discarding
    the original term name we now save it as an EXACT synonym with
    "PomBase_original_PRO_term_name: " as a prefix.
    
    This is a hack to allow us to include the original term name in
    column 3 of the GPI file.  :-(
    
    Refs pombase/curation#2851
    Refs pombase/pombase-chado#1173

--- etc/process_pombe_mini_pr.pl
+++ etc/process_pombe_mini_pr.pl
@@ -6,12 +6,17 @@ use warnings;
 $/ = '';
 
 while (<>) {
-  if (/^\s*synonym: "(.*)" EXACT PomBase_display_name \[.*\]\s*$/m) {
-    my $display_name = $1;
-    if (length $display_name > 0) {
-      s/^name:\s*(.*?)\s*$/name: $display_name/m;
+  s/\n+\z/\n/;
+  if (/^name:\s*(.*?)\s*$/m) {
+    my $term_name = $1;
+    if (/^\s*synonym: "(.*)" EXACT PomBase_display_name \[.*\]\s*$/m) {
+      my $display_name = $1;
+      if (length $display_name > 0) {
+        s/^name:\s*(.*?)\s*$/name: $display_name/m;
+      }
+      $_ .= qq|synonym: "PomBase_original_PRO_term_name: $term_name" EXACT [PomBase:auto]\n|;
     }
   }
-
-  print;
+  
+  print "$_\n";
 }
