8795dd6e07420b5a18fe3fd7d3d399fba4717ffd
Kim Rutherford <kmr@aska.gen.nz>
2024-05-25 03:31:43 +1200

    Load the GO-CAM ID to term ID mapping file
    Refs pombase/website#2173

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -622,7 +622,13 @@ $POMBASE_CHADO/script/pombase-import.pl $POMBASE_LEGACY/load-pombase-chado.yaml 
     --property-name="gocam_id" --organism-taxonid=4896 \
     --feature-uniquename-column=1 --property-column=2 \
     "$HOST" $DB $USER $PASSWORD < $SOURCES/pombe-embl/supporting_files/production_gocam_id_mapping.tsv \
-    2>&1 | tee -a $LOG_DIR/$log_file.production-gocam-id-mapping-file
+    2>&1 | tee -a $LOG_DIR/$log_file.production-gocam-gene-id-mapping-file
+
+echo loading GO-CAM ID to term ID mapping
+$POMBASE_CHADO/script/pombase-import.pl $POMBASE_LEGACY/load-pombase-chado.yaml generic-cvtermprop \
+    --property-name="gocam_id" --termid-column=1 --property-value-column=2 \
+    "$HOST" $DB $USER $PASSWORD < $SOURCES/pombe-embl/supporting_files/production_gocam_term_id_mapping.tsv \
+    2>&1 | tee -a $LOG_DIR/$log_file.production-gocam-term-id-mapping-file
 
 
 refresh_views
