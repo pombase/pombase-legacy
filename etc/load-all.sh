e176b8cdfcf0c8f1e9023cf288b1ede489fcbcba
Kim Rutherford <kmr@aska.gen.nz>
2024-09-04 14:35:25 +1200

    Remove duplicate disease slim file
    Refs pombase/website#2224

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -1139,7 +1139,6 @@ then
 
     cp $CURRENT_BUILD_DIR/misc/FYPOviability.tsv           $SOURCES/pombe-embl/ftp_site/pombe/annotations/Phenotype_annotations/FYPOviability.tsv
     cp $CURRENT_BUILD_DIR/misc/transmembrane_domain_coords_and_seqs.tsv    $SOURCES/pombe-embl/ftp_site/pombe/Protein_data/transmembrane_domain_coords_and_seqs.tsv
-    cp $CURRENT_BUILD_DIR/misc/pombe_mondo_slim_ids_and_names.tsv          $SOURCES/pombe-embl/ftp_site/pombe/documents/pombe_mondo_slim_ids_and_names.tsv
 
     cp $CURRENT_BUILD_DIR/exports/pombase-go-physical-interactions.tsv.gz  $SOURCES/pombe-embl/ftp_site/pombe/high_confidence_physical_interactions/
     cp $CURRENT_BUILD_DIR/exports/pombase-go-substrates.tsv.gz             $SOURCES/pombe-embl/ftp_site/pombe/high_confidence_physical_interactions/
