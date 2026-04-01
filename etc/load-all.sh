7e9ad6965b56362a265e97e7943f400a3e259923
Kim Rutherford <kmr@aska.gen.nz>
2026-04-01 18:50:30 +1300

    Write annotation from GO-CAMs, add to SVN
    Refs pombase/pombase-chado#1340

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -111,6 +111,9 @@ fi
 
 (cd $POMBE_EMBL
  $POMCUR/bin/pombase-gocam-tool overlapping-nodes supporting_files/noctua-go-cam-models/*.json | (sed -u 1q; sort) > supporting_files/nightly_load_results/overlapping_nodes.tsv
+ $POMCUR/bin/pombase-gocam-tool write-annotation --db-name PomBase \
+     --config-file-name $LOAD_CONFIG \
+     supporting_files/gocam-py-noctua-models/*.yaml > supporting_files/annotations-from-gocams.gaf.tsv
  svn status |
      perl -ne 'print "$1\n" if m:^!\s+(supporting_files/noctua-go-cam-models/.*\.json$|supporting_files/gocam-py-noctua-models/.*\.yaml$):' |
      xargs --no-run-if-empty svn rm
