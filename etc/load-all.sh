b9ea7900f7b5d08d8a81fdf52af562268fc5920d
Kim Rutherford <kmr@aska.gen.nz>
2026-04-29 18:37:23 +1200

    Update allele_summaries.json in SVN each night
    Refs pombase/pombase-chado#1350

--- etc/load-all.sh
+++ etc/load-all.sh
@@ -1352,6 +1352,8 @@ then
     cp $CURRENT_BUILD_DIR/misc/FYPOviability.tsv           $SOURCES/pombe-embl/ftp_site/pombe/annotations/Phenotype_annotations/FYPOviability.tsv
     cp $CURRENT_BUILD_DIR/misc/transmembrane_domain_coords_and_seqs.tsv    $SOURCES/pombe-embl/ftp_site/pombe/Protein_data/transmembrane_domain_coords_and_seqs.tsv
 
+    cp $CURRENT_BUILD_DIR/misc/allele_summaries.json $SOURCES/pombe-embl/supporting_files/nightly_load_results/allele_summaries.json
+
     cp $CURRENT_BUILD_DIR/exports/pombase-go-physical-interactions.tsv.gz  $SOURCES/pombe-embl/ftp_site/pombe/high_confidence_physical_interactions/
     cp $CURRENT_BUILD_DIR/exports/pombase-go-substrates.tsv.gz             $SOURCES/pombe-embl/ftp_site/pombe/high_confidence_physical_interactions/
 
